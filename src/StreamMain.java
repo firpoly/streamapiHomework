4f1855b83d9df4772f3c2062b81133c34a6b918d
Firsova Polina <firpoly@gmail.com>
2024-02-04 21:13:46 +0300

    внесение правок в первом задании

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamMain {
@@ -7,5 +9,12 @@ public class StreamMain {
                 filter(x -> x % 2 == 0).
                 sorted(Comparator.naturalOrder()).
                 forEach(System.out::println);
+    }
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> stream = intList.stream();
+        StreamMain workStream = new StreamMain();
+        System.out.println("работа c Stream API");
+        workStream.workStreamAPI(stream);
     }
 }
