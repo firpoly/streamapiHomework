78e628f72499a4b9adca3ef2aaacdd0ca15352d5
Firsova Polina <firpoly@gmail.com>
2024-02-06 13:34:25 +0300

    убраны методы

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,17 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Main {
-    public static void withoutStreamAPI(List<Integer> arrays) {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         List<Integer> copyList = new ArrayList<>();
-        for (int i = 0; i < arrays.size(); i++) {
-            if ((arrays.get(i) > 0) && (arrays.get(i) % 2 == 0)){
-                copyList.add(arrays.get(i));
+        for (int i = 0; i < intList.size(); i++) {
+            if ((intList.get(i) > 0) && (intList.get(i) % 2 == 0)){
+                copyList.add(intList.get(i));
             }
         }
         copyList.sort(Comparator.naturalOrder());
         for (Integer item : copyList) {
             System.out.println(item);
         }
-    }
-    public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        System.out.println("работа без Stream API");
-        withoutStreamAPI(intList);
     }
 }
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -4,17 +4,13 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamMain {
-    public void workStreamAPI(Stream<Integer> stream) {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> stream = intList.stream();
         stream.filter(x -> x > 0).
                 filter(x -> x % 2 == 0).
                 sorted(Comparator.naturalOrder()).
                 forEach(System.out::println);
-    }
-    public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Stream<Integer> stream = intList.stream();
-        StreamMain workStream = new StreamMain();
-        System.out.println("работа c Stream API");
-        workStream.workStreamAPI(stream);
     }
 }
